92be1b5a3d70ae4ceed502c82c6c6ee6f939771c
NCLC365 <nclc365bnpmedia@gmail.com>
2025-03-18 06:14:09 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
+  trailingSlash: true,  // Ensures all routes work properly
+  images: {
+    unoptimized: true, // Prevents image build errors on Netlify
+  },
 };
+
+module.exports = nextConfig;
