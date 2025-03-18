2d5d8a44327d6c29454f242bbf03933d8f776b27
NCLC365 <nclc365bnpmedia@gmail.com>
2025-03-18 06:44:06 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full p-4 text-center bg-gray-200 text-gray-700 mt-8">
+      &copy; {new Date().getFullYear()} NCLC365. All rights reserved.
+    </footer>
+  );
+}
