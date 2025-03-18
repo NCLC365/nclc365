58dff5dc8e2b5dd135263a11088fecdc8b74d68c
NCLC365 <nclc365bnpmedia@gmail.com>
2025-03-18 06:42:09 +0530

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="w-full bg-blue-600 text-white py-4">
+      <div className="container mx-auto flex justify-between items-center px-6">
+        <h1 className="text-xl font-bold">NCLC365</h1>
+        <div className="space-x-6">
+          <Link href="/" className="hover:underline">Home</Link>
+          <Link href="/about" className="hover:underline">About</Link>
+          <Link href="/tools" className="hover:underline">Tools</Link>
+          <Link href="/contact" className="hover:underline">Contact</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
