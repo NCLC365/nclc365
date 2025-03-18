70213e389ab8981ed770382063ede5b101d65115
NCLC365 <nclc365bnpmedia@gmail.com>
2025-03-18 22:02:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true, // Ensures URLs work correctly
+  output: "export", // Generates static files for Netlify
+
   experimental: {
-    // Remove 'appDir' if you're not using the new app directory
+    // Remove 'appDir' if not using the new app directory
   },
   compiler: {
     styledComponents: true, // If using styled-components
